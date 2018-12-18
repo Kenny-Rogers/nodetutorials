21efe7b39425ce16a1e34276c45931fb021abaeb
kenny-rogers <ylodonu@gmail.com>
2018-12-18 17:38:37 +0000

    working with post request

--- workingWithExpress/app.js
+++ workingWithExpress/app.js
@@ -2,15 +2,21 @@
 const express = require('express');
 //including path module
 const path = require('path');
+//including the bodyParser module that allows us to access form data
+const bodyParser = require('body-parser');
 
 //app object is created by express and contains methods that can be used
 const app = express();
 
-//
+//using a middleware that creates an alias for our static files
+app.use('/public',express.static(path.join(__dirname,'static')));
+//using a middleware to allow parsing of urlencoded forms
+app.use(bodyParser.urlencoded({extended:false}));
 
 //creating get request for the home or index page
 app.get('/',(request, response)=>{
-    response.send('Hello World');
+    //response.send('Hello World');
+    response.sendFile(path.join(__dirname,'static','index.html'));
 });
 
 //creating a route with url parameters
@@ -22,6 +28,13 @@ app.get('/example/:name/:age',(request,response)=>{
     //sending response
     response.send("Username is "+ request.params.name + 
                     "\nAge is "+ request.params['age']);
+});
+
+//handling POST request
+app.post('/',(request, response)=>{
+    console.log(request.body);
+    //database work here
+    response.send('successfully posted data');
 });
 
 //setting up a port to listen for request on
