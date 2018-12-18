efcbcec15f7fec3bf78a8ebcea4802f1b8fd7dc7
kenny-rogers <ylodonu@gmail.com>
2018-12-18 05:45:25 +0000

    working with filesystem

--- filesystem2.js
+++ filesystem2.js
@@ -14,14 +14,37 @@ fs.mkdir('tutorial',function(err){
                 console.log('successfully created file');
         });
 
-        // //deleting a folder
-        // fs.rmdir('tutorial',(err)=>{
-        //     if(err)
-        //         console.log(err);
-        //     else 
-        //         console.log('successfully deleted');
-        // })
+        //deleting a folder
+        fs.rmdir('tutorial',(err)=>{
+            if(err)
+                console.log(err);
+            else 
+                console.log('successfully deleted');
+        })
     }
 });
 
 //deleting a folder with files in it
+fs.readdir('./tutorial',(err, files)=>{
+    if(err)
+        console.log(err);
+    else {
+        //delete files in folder
+        for(let file of files){
+          fs.unlink('./tutorial/'+file,(err)=>{
+            if(err)
+              console.log(err);
+            else
+              console.log('successfully deleted files');
+          });
+        }
+
+        //delete folder
+        fs.rmdir('./tutorial', (err)=>{
+          if(err)
+            console.log(err);
+          else
+            console.log('Successfully deleted folder');
+        });
+    }
+});
